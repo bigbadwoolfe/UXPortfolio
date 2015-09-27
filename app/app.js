0c0eab7ab425a4dccd1f63d90dd17ca03b5dede3
Metodi Baniski <baniski@gmail.com>
2015-09-27 15:03:20 -0400

    Optimizing navigation for speed, screen responsiveness.

--- app/app.js
+++ app/app.js
@@ -14,4 +14,51 @@ angular.module('mbPortfolio', [
   $routeProvider.when('/', {
     redirectTo: '/about'
   });
+}]).controller('SiteController', ['$scope','$rootScope','$location','$filter', '$timeout', function($scope, $rootScope, $location, $filter, $timeout) {
+
+  $scope.model = $scope.fn = {};
+  $scope.model.navItems = [
+    {href:'#/contact', class:'contact', text:'Contact', icon:'at', id:'contact'},
+    {href:'#/resume', class:'resume', text:'Resume', icon:'file-text', id:'resume'},
+    {href:'#/portfolio', class:'portfolio', text:'Portfolio', icon:'file-image-o', id:'portfolio'},
+    {href:'#/process', class:'process', text:'Process', icon:'cogs', id:'process'},
+    {href:'#/about', class:'about', text:'About', icon:'user', id:'about'}
+  ];
+
+  var cActive = $scope.model.activeNavItem = getCurrentNavItem();
+
+  $scope.setActive = function(id) {
+    $scope.model.activeNavItem = getNavItemById(id);
+    cActive = $scope.model.activeNavItem;
+  };
+
+  $scope.showHover = function(id) {
+    cActive = $scope.model.activeNavItem;
+    $scope.model.activeNavItem = getNavItemById(id);
+  };
+
+  $scope.hideHover = function() {
+    $scope.model.activeNavItem = cActive;
+  }
+
+  function getNavItemById(id) {
+    var a = $filter('filter')($scope.model.navItems, {id: id});
+    if (a.length == 1)
+      return a[0];
+    return;
+  }
+
+  function getNavItemByUrl(url) {
+    var a = $filter('filter')($scope.model.navItems, {href: '#' + url});
+    if (a.length == 1)
+      return a[0];
+    return;
+  }
+
+  function getCurrentNavItem() {
+    return getNavItemByUrl($location.path());
+  }
+
+
+
 }]);
--- app/components/header/header.ctrl.js
+++
@@ -1,8 +0,0 @@
-angular.module('mbPortfolio.components')
-    .controller('HeaderController',['$scope', '$rootScope',function($scope, $rootScope){
-        $scope.title='About';
-
-        $scope.$on('headerContentChanged', function(event, arg) {
-            $scope.title = arg.title;
-        });
-    }]);
