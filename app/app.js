4480150c27292d0dd4bd56ab899423f40b165f3c
Metodi Baniski <baniski@gmail.com>
2015-09-28 16:49:16 -0400

    Setting active page explicitly on 'about' --- page doesn't always get set on a redirect.

--- app/app.js
+++ app/app.js
@@ -2,63 +2,65 @@
 
 // Declare app level module which depends on views, and components
 angular.module('mbPortfolio', [
-  'ngRoute',
-  'ui.bootstrap',
-  'mbPortfolio.about',
-  'mbPortfolio.process',
-  'mbPortfolio.portfolio',
-  'mbPortfolio.resume',
-  'mbPortfolio.contact',
-  'mbPortfolio.components'
+    'ngRoute',
+    'ui.bootstrap',
+    'mbPortfolio.about',
+    'mbPortfolio.process',
+    'mbPortfolio.portfolio',
+    'mbPortfolio.resume',
+    'mbPortfolio.contact',
+    'mbPortfolio.components'
 ]).config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {
-    redirectTo: '/about'
-  });
+    $routeProvider.when('/', {
+        redirectTo: '/about'
+    });
 }]).controller('SiteController', ['$scope','$rootScope','$location','$filter', '$timeout', function($scope, $rootScope, $location, $filter, $timeout) {
 
-  $scope.model = $scope.fn = {};
-  $scope.model.navItems = [
-    {href:'#/contact', class:'contact', text:'Contact', icon:'at', id:'contact'},
-    {href:'#/resume', class:'resume', text:'Resume', icon:'file-text', id:'resume'},
-    {href:'#/portfolio', class:'portfolio', text:'Portfolio', icon:'file-image-o', id:'portfolio'},
-    {href:'#/process', class:'process', text:'Process', icon:'cogs', id:'process'},
-    {href:'#/about', class:'about', text:'About', icon:'user', id:'about'}
-  ];
+    $scope.model = $scope.fn = {};
+    $scope.model.navItems = [
+        {href:'#/contact', class:'contact', text:'Contact', icon:'at', id:'contact'},
+        {href:'#/resume', class:'resume', text:'Resume', icon:'file-text', id:'resume'},
+        {href:'#/portfolio', class:'portfolio', text:'Portfolio', icon:'file-image-o', id:'portfolio'},
+        {href:'#/process', class:'process', text:'Process', icon:'cogs', id:'process'},
+        {href:'#/about', class:'about', text:'About', icon:'user', id:'about'}
+    ];
 
-  var cActive = $scope.model.activeNavItem = getCurrentNavItem();
+    var cActive = $scope.model.activeNavItem = getCurrentNavItem();
+    $scope.setActive = function(id) {
+        $scope.model.activeNavItem = getNavItemById(id);
+        cActive = $scope.model.activeNavItem;
+    };
 
-  $scope.setActive = function(id) {
-    $scope.model.activeNavItem = getNavItemById(id);
-    cActive = $scope.model.activeNavItem;
-  };
+    $rootScope.$on('page-changed', function(evt,data) {
+        $scope.setActive(data);
+    });
+    $scope.showHover = function(id) {
+        cActive = $scope.model.activeNavItem;
+        $scope.model.activeNavItem = getNavItemById(id);
+    };
 
-  $scope.showHover = function(id) {
-    cActive = $scope.model.activeNavItem;
-    $scope.model.activeNavItem = getNavItemById(id);
-  };
+    $scope.hideHover = function() {
+        $scope.model.activeNavItem = cActive;
+    }
 
-  $scope.hideHover = function() {
-    $scope.model.activeNavItem = cActive;
-  }
+    function getNavItemById(id) {
+        var a = $filter('filter')($scope.model.navItems, {id: id});
+        if (a.length == 1)
+            return a[0];
+        return;
+    }
 
-  function getNavItemById(id) {
-    var a = $filter('filter')($scope.model.navItems, {id: id});
-    if (a.length == 1)
-      return a[0];
-    return;
-  }
+    function getNavItemByUrl(url) {
+        var a = $filter('filter')($scope.model.navItems, {href: '#' + url});
+        if (a.length == 1)
+            return a[0];
+        return;
+    }
 
-  function getNavItemByUrl(url) {
-    var a = $filter('filter')($scope.model.navItems, {href: '#' + url});
-    if (a.length == 1)
-      return a[0];
-    return;
-  }
+    function getCurrentNavItem() {
+        return getNavItemByUrl($location.path());
 
-  function getCurrentNavItem() {
-    return getNavItemByUrl($location.path());
-
-  }
+    }
 
 
 
--- app/pages/about/about.js
+++ app/pages/about/about.js
@@ -2,13 +2,13 @@
 
 angular.module('mbPortfolio.about', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/about', {
-    templateUrl: 'pages/about/about.html',
-    controller: 'AboutCtrl'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/about', {
+        templateUrl: 'pages/about/about.html',
+        controller: 'AboutCtrl'
+      });
+    }])
 
-.controller('AboutCtrl', [function() {
-
-}]);
+    .controller('AboutCtrl', ['$rootScope', function($rootScope) {
+      $rootScope.$broadcast('page-changed', 'about');
+    }]);
